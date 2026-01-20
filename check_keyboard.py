af6e956b192d97bf5369de9fbd370b53b48a9887
Dwarakesh-V <vdwarakesh0211@gmail.com>
2026-01-20 18:34:11 +0530

    Removed root conditions and uinput keyboards for privacy and layout issues

--- check_keyboard.py
+++ check_keyboard.py
@@ -1,17 +1,6 @@
-import os
-
-# Check if running as superuser
-if os.geteuid() == 0:
-    # Running as root/sudo - use uinput
-    import uinput_keyboard
-    uinput_keyboard.init()
-    uinput_keyboard.type_text("Hello World!")
-    uinput_keyboard.cleanup()
-    print("Root user - using uinput")
-else:
-    # Running as normal user - use X11
-    import x11_keyboard
-    x11_keyboard.init()
-    x11_keyboard.type_text("Hello World!")
-    x11_keyboard.cleanup()
-    print("Non root user - using x11 (May not work on wayland)")
+# Works without root
+import x11_keyboard
+x11_keyboard.init()
+x11_keyboard.type_text("Hello World!")
+x11_keyboard.cleanup()
+print("Non root user - using x11 (May not work on wayland)")
--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-# from setuptools import setup, Extension
-
-# module = Extension('uinput_keyboard',
-#                    sources=['uinput_keyboard.cpp'],
-#                    extra_compile_args=['-std=c++11'])
-
-# setup(name='uinput_keyboard',
-#       version='1.0',
-#       description='Linux uinput keyboard simulator',
-#       ext_modules=[module])
-
 from setuptools import setup, Extension
 
 module = Extension('x11_keyboard',
